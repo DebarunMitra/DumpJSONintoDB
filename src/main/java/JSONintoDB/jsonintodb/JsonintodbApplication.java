264b3e0cece02fb183df03f166c00d7c60703e16
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2021-03-08 17:13:23 +0530

    initial setup added

---
+++ src/main/java/JSONintoDB/jsonintodb/JsonintodbApplication.java
@@ -0,0 +1,13 @@
+package JSONintoDB.jsonintodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonintodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonintodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JSONintoDB/jsonintodb/JsonintodbApplicationTests.java
@@ -0,0 +1,13 @@
+package JSONintoDB.jsonintodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonintodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
