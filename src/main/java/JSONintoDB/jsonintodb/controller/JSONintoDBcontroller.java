133df69b87d3927ba1b46575f597f73f6e723c25
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2021-03-09 17:39:28 +0530

    controller class created

---
+++ src/main/java/JSONintoDB/jsonintodb/controller/JSONintoDBcontroller.java
@@ -0,0 +1,10 @@
+package JSONintoDB.jsonintodb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/dump")
+public class JSONintoDBcontroller {
+    
+}
