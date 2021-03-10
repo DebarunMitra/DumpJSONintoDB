a80c81e034a576836359e66b8383874dd8e4378c
Debarun Mitra <50083511+DebarunMitra@users.noreply.github.com>
2021-03-10 17:54:51 +0530

    server updated

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.use(cors());
 app.use(express.json());
 
 const CoursesRoutes = require('./routes/courses');
+const SyncRoutes = require('./routes/sync');
 
 app.use('/dump/api/v1/', CoursesRoutes);
+app.use('/dump/api/v1/sync/', SyncRoutes);
 
 app.listen(port, () => {
     console.log(`listening ${port}`);
